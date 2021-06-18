284ad597071339cdd4a73f1856940edfec29de10
cxw9 <1541649163@qq.com>
2021-06-18 18:17:45 +0800

    222

--- api/pic.js
+++ api/pic.js
@@ -1,4 +1,15 @@
 module.exports = (req, res) => {
-	url='https://tva3.sinaimg.cn/large/87c01ec7gy1fsnqqshib2j21kw0w0apc.jpg'
-	res.redirect(url)
+  // npm install --save random-int
+  var randomInt = require('random-int');	
+  var mid=randomInt(1, 1000);
+  var fs = require('fs');	
+  var path = require("path")
+  var file = path.resolve('api_runrab.db');;  //这里写的就是数据库文件的路径  
+  //var exists = fs.existsSync(file);  
+  //console.log(exists);
+  var Database = require('better-sqlite3');
+  var db = new Database(file,{ verbose: console.log });	
+  var row = db.prepare('SELECT url FROM img WHERE id=?',mid).all();
+  db.close();
+  res.redirect(row[0].url)
 }
