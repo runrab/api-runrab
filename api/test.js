d47369cd99bb749bd4504ddcdcc7d7cb07eb88ba
cxw9 <1541649163@qq.com>
2021-06-18 15:30:40 +0800

    1

--- api/test.js
+++ api/test.js
@@ -12,10 +12,10 @@
 	
 }*/
 module.exports = (req, res) => {
-  const db = require('better-sqlite3')('./api_runrab.db',options);
+  const db = require('better-sqlite3')('./api_runrab.db');
   const row = db.prepare('SELECT * FROM img WHERE id=10');
   console.log("test");
-  //console.log(row.url); 
+  console.log(row.url); 
   //db.close();
   //url= row.url
   //res.redirect(url);	
