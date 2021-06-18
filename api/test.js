d00e7acddbb79dbb22c2e66f34fd1bf76222f844
cxw9 <1541649163@qq.com>
2021-06-18 15:58:15 +0800

    1

--- api/test.js
+++ api/test.js
@@ -13,12 +13,12 @@
 }*/
 module.exports = (req, res) => {
   var fs = require('fs');	
-  var file = '../api_runrab.db';//这里写的就是数据库文件的路径  
+  var file = 'api_runrab.db';//这里写的就是数据库文件的路径  
   var exists = fs.existsSync(file);  
   console.log(exists);
-  const Database = require('better-sqlite3');
-  const db = new Database(file,{ verbose: console.log });	
-  const row = db.prepare('SELECT url FROM img WHERE id=10');
+  var Database = require('better-sqlite3');
+  var db = new Database(file,{ verbose: console.log });	
+  var row = db.prepare('SELECT url FROM img WHERE id=10');
   //console.log("test");
   //console.log(row.url); 
   //db.close();
