16babc84b98087c021c77458bea5493473991dda
cxw9 <1541649163@qq.com>
2021-06-20 16:06:06 +0800

    2

--- api/img/json.js
+++ api/img/json.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
     const SqliteDB = require('./better.js').SqliteDB;
     let file = path.resolve('api_runrab.db');
     const sqliteDB = new SqliteDB(file);
-    const mid=300;
+    const mid=Math.random()*1000 + 1;
     //多个的话 mid 为数组 [ , ]
     const querySql='SELECT * FROM img where id=?';
     sqliteDB.queryData(querySql, mid,dataDeal);
