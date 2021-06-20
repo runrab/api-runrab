cd9403d8eed1a0add5706ce8d01165641aa5ee1b
cxw9 <1541649163@qq.com>
2021-06-20 16:02:59 +0800

    1

--- api/img/pic.js
+++ api/img/pic.js
@@ -20,7 +20,7 @@ module.exports = (req, res) => {
   let file = path.resolve('api_runrab.db');
 //var file = "api_runrab.db";
   const sqliteDB = new SqliteDB(file);
-  const mid=Math.random.floor(1000)+1;
+  const mid=300;
 //sql区域==========
   const querySql='SELECT * FROM img where id=?';
   let picUrl = sqliteDB.queryData(querySql,mid, dataDeal);
