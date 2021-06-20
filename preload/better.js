ff9661765e10383f5c1dc1b74d0f85890b8f6b06
cxw9 <1541649163@qq.com>
2021-06-20 19:47:43 +0800

    规范代码
    回滚暂定

--- preload/better.js
+++ preload/better.js
@@ -1,9 +1,8 @@
-/**
- const fs = require('fs');
- const path = require("path");
- const file = path.resolve('../api_runrab.db');
- var DB = DB || {};
- const database = require('better-sqlite3');
+const fs = require('fs');
+const path = require("path");
+const file = path.resolve('../api_runrab.db');
+var DB = DB || {};
+const database = require('better-sqlite3');
 
  DB.SqliteDB = function(file){
     DB.db = new database(file);
@@ -53,78 +52,6 @@
     }
 }
 
-
- exports.SqliteDB = DB.SqliteDB;
-
- */
-const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
-
-var DB = DB || {};
-
-DB.SqliteDB = function(file){
-    DB.db = new sqlite3.Database(file);
-
-    DB.exist = fs.existsSync(file);
-    if(!DB.exist){
-        console.log('数据库文件不存在或者位置不对');
-        //console.log("Creating db file!");
-        //fs.openSync(file, 'w');
-    };
-};
-
-DB.printErrorInfo = function(err){
-    console.log("Error Message:" + err.message + " ErrorNumber:" + err);
-};
-
-DB.SqliteDB.prototype.createTable = function(sql){
-    DB.db.serialize(function(){
-        DB.db.run(sql, function(err){
-            if(null != err){
-                DB.printErrorInfo(err);
-                return;
-            }
-        });
-    });
-};
-
-/// tilesData format; [[level, column, row, content], [level, column, row, content]]
-DB.SqliteDB.prototype.insertData = function(sql, objects){
-    DB.db.serialize(function(){
-        var stmt = DB.db.prepare(sql);
-        for(var i = 0; i < objects.length; ++i){
-            stmt.run(objects[i]);
-        }
-        stmt.finalize();
-    });
-};
-
-DB.SqliteDB.prototype.queryData = function(sql,mid, callback){
-    DB.db.all(sql, mid,function(err, rows){
-        if(null != err){
-            DB.printErrorInfo(err);
-            return;
-        }
-        /// deal query data.
-        if(callback){
-            callback(rows);
-        }
-    });
-};
-
-DB.SqliteDB.prototype.executeSql = function(sql){
-    DB.db.run(sql, function(err){
-        if(null != err){
-            DB.printErrorInfo(err);
-        }
-    });
-};
-
-DB.SqliteDB.prototype.close = function(){
-    DB.db.close();
-};
-
-/// export SqliteDB.
-exports.SqliteDB = DB.SqliteDB;
+//exports.SqliteDB = DB.SqliteDB;
 
 
