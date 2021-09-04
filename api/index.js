7841bfe31019cc270981901732d883a806923d9f
runrab <51315711+runrab@users.noreply.github.com>
2021-09-04 16:30:19 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,17 @@
 module.exports = (req, res) => {
-  var fs = require('fs');
-  var path = require("path")
-  var file = path.resolve('api_runrab.db');;  //这里写的就是数据库文件的路径
-  var exists = fs.existsSync(file);
-  console.log(exists);
-  var Database = require('better-sqlite3');
-  var db = new Database(file,{ verbose: console.log });
-  var row = db.prepare('SELECT url FROM img WHERE id=100').all();
-  db.close();
-  console.log(row);
-  console.log("test");
-  //console.log(typeof row[0],typeof JSON.parse(row[0],JSON.parse(row[0]);
-  //console.log(typeof row[0].url);
-  //var surl = JSON.parse(row[0]);
-  //db.close();
-  //url= row.url
-  //res.redirect(url);
-  //console.log(row);
-  res.redirect(row[0].url)
+  /**
+   * precision 精度 100 1000 10000
+   */
+  function getNumberUid(precision){
+    const rawPre = (Date.now() - new Date(1624206802955).getTime()) / precision;
+    const preNumber = Number(rawPre.toFixed()) * precision;
+    const randam = Math.floor(Math.random() * precision);
+    return preNumber + randam;
+  }
+
+  const db = require('better-sqlite3')('api_runrab.db', options);
+  const row = db.prepare('SELECT * FROM users WHERE id=?').get(getNumberUid(2000));
+  console.log(row.url);
 }
 
 
