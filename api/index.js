c50c6b94641b99594b7addfacc3d7f02d4dbe352
runrab <51315711+runrab@users.noreply.github.com>
2021-09-04 16:37:51 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,4 @@
 module.exports = (req, res) => {
-  /**
-   * precision 精度 100 1000 10000
-   */
-  function getNumberUid(precision){
-    const rawPre = (Date.now() - new Date(1624206802955).getTime()) / precision;
-    const preNumber = Number(rawPre.toFixed()) * precision;
-    const randam = Math.floor(Math.random() * precision);
-    return preNumber + randam;
-  }
-  const db = require('better-sqlite3')('api_runrab.db', options);
-  const row = db.prepare('SELECT * FROM users WHERE id=?').get(200);
-  console.log("aaa");
-}
-
-
-
-
-
-
-/**module.exports = (req, res) => {
   const { name = 'World' } = req.query
   res.send(`Hello ${name}!`)
 }
-*/
